21a78c238bddb3e1e29accfa71be65cbb310225b
diegoor <diegoggonzales7@gmail.com>
2023-01-02 17:40:18 -0500

    Updates

---
+++ precache-manifest.c520474545a6817e1b69029f3d69a354.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db15c30aa9632f1f750a221646442cd2",
+    "url": "/ecollect/index.html"
+  },
+  {
+    "revision": "ec8b07151e50a48b840e",
+    "url": "/ecollect/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "553c94ee580fa4ac6747",
+    "url": "/ecollect/static/css/main.36ed1cb9.chunk.css"
+  },
+  {
+    "revision": "ec8b07151e50a48b840e",
+    "url": "/ecollect/static/js/2.4e091c0d.chunk.js"
+  },
+  {
+    "revision": "553c94ee580fa4ac6747",
+    "url": "/ecollect/static/js/main.3d031f10.chunk.js"
+  },
+  {
+    "revision": "3810410e19dd1470093b",
+    "url": "/ecollect/static/js/runtime~main.36df8736.js"
+  },
+  {
+    "revision": "ff1903b2f72fb064f261806a06c56a47",
+    "url": "/ecollect/static/media/calidad.ff1903b2.png"
+  },
+  {
+    "revision": "0da104b4ab43cc3503cc934a1adfd645",
+    "url": "/ecollect/static/media/carrusel1.0da104b4.png"
+  },
+  {
+    "revision": "3945b2f9eb23af694e704f086c869aab",
+    "url": "/ecollect/static/media/carrusel2.3945b2f9.png"
+  },
+  {
+    "revision": "a950df9b73830daecdab23d459bc7e6f",
+    "url": "/ecollect/static/media/carrusel3.a950df9b.png"
+  },
+  {
+    "revision": "c3f9dcb7d0cbc241da6200b7bb44761c",
+    "url": "/ecollect/static/media/comentarioJ.c3f9dcb7.PNG"
+  },
+  {
+    "revision": "0b3757a0e5269a323102694d78d61593",
+    "url": "/ecollect/static/media/comentarioL.0b3757a0.PNG"
+  },
+  {
+    "revision": "91c32d2af906885c8d698a2a700e7f09",
+    "url": "/ecollect/static/media/comentarioM.91c32d2a.PNG"
+  },
+  {
+    "revision": "7df30611ca44dc45cb9f741201023667",
+    "url": "/ecollect/static/media/finalizado.7df30611.png"
+  },
+  {
+    "revision": "b2e449f39c553f89cd4465be80f5f593",
+    "url": "/ecollect/static/media/logotipo_original.b2e449f3.png"
+  },
+  {
+    "revision": "cf612db8276f71d2ae0fa8adb01bb5b3",
+    "url": "/ecollect/static/media/reciclaje.cf612db8.png"
+  },
+  {
+    "revision": "6566d5d680ae24332cf63c40cee16a9c",
+    "url": "/ecollect/static/media/registro.6566d5d6.png"
+  },
+  {
+    "revision": "07b34fdfafa1b3f6324b461a73828631",
+    "url": "/ecollect/static/media/seguridad.07b34fdf.png"
+  }
+]);
